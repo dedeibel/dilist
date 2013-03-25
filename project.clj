9ac63c2897920cbaec77c249d480353a66c88076
Benjamin Peter <benjaminpeter@arcor.de>
2013-03-25 21:40:54 +0100

    Raise dependency versions. Clojure and clj-pls.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.0"]
                  [clj-http "0.5.4"]
                  [enlive "1.0.1"]
                  [org.clojure/tools.trace "0.7.5"]
-                 [clj-pls "0.2.0"]
+                 [clj-pls "0.2.2"]
                  [org.ini4j/ini4j "0.5.2"]           ; java
                  ]
   :repositories [["mavenrepository" "http://repo1.maven.org/maven2/"]]
   :profiles { :dev {
-                    :dependencies [[midje "1.5-RC1"]]
-                    :plugins [[lein-midje "3.0-RC1"]]}}
+                    :dependencies [[midje "1.5.1"]]
+                    :plugins [[lein-midje "3.0.1"]]}}
   :main omniplaylist.core)
