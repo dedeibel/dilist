8d55feba1df557de596bfc7bf225803e8535abc0
Benjamin Peter <benjaminpeter@arcor.de>
2013-05-06 21:23:17 +0200

    Changed clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.5.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [clj-http "0.5.4"]
                  [enlive "1.0.1"]
                  [org.clojure/tools.trace "0.7.5"]
