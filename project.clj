ff8ef8cf92a743ff28bf5d25eace6be5841dea99
Benjamin Peter <benjaminpeter@arcor.de>
2013-06-30 16:13:44 +0200

    Added old leiningen dev dependencies for travis.

--- project.clj
+++ project.clj
@@ -12,6 +12,9 @@
                  [org.ini4j/ini4j "0.5.2"]           ; java
                  ]
   :repositories [["mavenrepository" "http://repo1.maven.org/maven2/"]]
+  ; deprecated but added for travis ci with old leiningen version
+  :dev-dependencies [[midje "1.5.1" :exclusions [org.clojure/clojure]]
+                     [lein-midje "3.0.1"]]
   :profiles { :dev {
                     :dependencies [[midje "1.5.1"]]
                     :plugins [[lein-midje "3.0.1"]]}}
