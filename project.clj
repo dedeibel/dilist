e42118245060b10dc7366733435bbdc9e93d87a6
Benjamin Peter <benjaminpeter@arcor.de>
2013-06-30 22:30:17 +0200

    Moved from snapshot 0.1.0 up to version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject omniplaylist "0.1.0-SNAPSHOT"
+(defproject omniplaylist "0.1.1"
   :description "Downloads some interesting playlists and provides one conactenated list"
   :url "http://benjaminpeter.name"
   :license {:name "Eclipse Public License"
