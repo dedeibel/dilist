a0bb446c441158dd4acae0d188d0ca581a06c70e
Benjamin Peter <benjaminpeter@arcor.de>
2013-07-26 20:43:45 +0200

    Increased clj-pls version to make pls output format compatible with windows systems that dont like space characters near the equal sign. Increased clj-http version because of two bugs with pooled connections.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.3"]
+                 [clj-http "0.7.6"]
                  [enlive "1.1.1"]
                  [org.clojure/tools.trace "0.7.5"]
-                 [clj-pls "0.2.2"]
+                 [clj-pls "0.2.3"]
                  [org.ini4j/ini4j "0.5.2"]           ; java
                  ]
   :repositories [["mavenrepository" "http://repo1.maven.org/maven2/"]]
